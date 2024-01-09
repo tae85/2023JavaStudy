3a415593c979f8d86322954cd816f68a1741e58a
tae85 <ekzmfldj1@gmail.com>
2024-01-09 17:07:44 +0900

    24_01_09

--- src/ex08class/QuMarbles.java
+++ src/ex08class/QuMarbles.java
@@ -1,19 +1,20 @@
 package ex08class;
 
 class ChildProperty {
-	int ball;
-	
+	int beads;
+
+	// 생성자는 외부에서 호출해야 하므로 반드시 public으로 지정한다.
 	public ChildProperty(int a) {
-		ball = a;
+		beads = a;
 	}
-	
+
 	void showProperty() {
-		System.out.println("보유 구슬의 개수 " + ball);
+		System.out.println("보유 구슬의 개수 " + beads);
 	}
-	
+
 	void obtainBead(ChildProperty child, int a) {
-		child.ball -= a;
-		ball += a;
+		this.beads += a;
+		child.beads -= a;
 	}
 }
 
@@ -25,19 +26,19 @@ public class QuMarbles {
 
 		System.out.println("게임 전 구슬의 보유 개수");
 		System.out.print("어린이1 : ");
-		child1.showProperty(); 
+		child1.showProperty();
 		System.out.print("어린이2 : ");
-		child2.showProperty(); 
+		child2.showProperty();
 
-		/*1차게임 : 어린이1은 어린이2의 구슬 5개 획득*/
+		/* 1차게임 : 어린이1은 어린이2의 구슬 5개 획득 */
 		child1.obtainBead(child2, 5);
 
-		/*2차게임 : 어린이2가 어린이1의 구슬 9개 획득*/
+		/* 2차게임 : 어린이2가 어린이1의 구슬 9개 획득 */
 		child2.obtainBead(child1, 9);
 
 		System.out.println("\n게임 후 구슬의 보유 개수");
 		System.out.print("어린이1 : ");
-		child1.showProperty(); 
+		child1.showProperty();
 		System.out.print("어린이2 : ");
 		child2.showProperty();
 	}
--- src/ex12inheritance/QuConstructorAndSuper.java
+++ src/ex12inheritance/QuConstructorAndSuper.java
@@ -1,37 +1,44 @@
 package ex12inheritance;
 
-class Car
-{
-       	int gasoline;
+class Car {
+	int gasoline;
+	
+	public Car(int g) {
+		this.gasoline = g;
+	}
 }
-class HybridCar extends Car
-{
-       	int electric;
+
+class HybridCar extends Car {
+	int electric;
+	
+	public HybridCar(int g, int e) {
+		super(g);
+		this.electric = e;
+	}
 }
-class HybridWaterCar extends HybridCar
-{
-       	int water;
-       	
-       	public HybridWaterCar(int g, int e, int w) {
-       		gasoline = g;
-       		electric = e;
-       		water = w;
-       	}
- 
-       	public void showNowGauge()
-       	{
-                 	System.out.println("남은가솔린:"+gasoline);
-                 	System.out.println("남은전기량:"+electric);
-                 	System.out.println("남은워터량:"+water);
-       	}
+
+class HybridWaterCar extends HybridCar {
+	int water;
+
+	public HybridWaterCar(int g, int e, int w) {
+		super(g, e);
+//		super.gasoline = g;	// 변수가 private 일 때는 안 됨
+//		super.electric = e;
+		this.water = w;
+	}
+
+	public void showNowGauge() {
+		System.out.println("남은가솔린:" + gasoline);
+		System.out.println("남은전기량:" + electric);
+		System.out.println("남은워터량:" + water);
+	}
 }
 
 public class QuConstructorAndSuper {
 
 	public static void main(String[] args) {
-		HybridWaterCar hcar = new HybridWaterCar(10,20,30);
+		HybridWaterCar hcar = new HybridWaterCar(10, 20, 30);
 		hcar.showNowGauge();
-
 
 	}
 
